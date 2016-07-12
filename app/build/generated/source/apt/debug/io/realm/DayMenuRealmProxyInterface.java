b22c134c03fb422fe8949405983dd89b913494e6
Nuno Neto <nuno.agn@gmail.com>
2016-07-12 20:16:29 +0100

    gitignore

--- app/build/generated/source/apt/debug/io/realm/DayMenuRealmProxyInterface.java
+++
@@ -1,9 +0,0 @@
-package io.realm;
-
-
-public interface DayMenuRealmProxyInterface {
-    public int realmGet$dayOfWeek();
-    public void realmSet$dayOfWeek(int value);
-    public String realmGet$description();
-    public void realmSet$description(String value);
-}
